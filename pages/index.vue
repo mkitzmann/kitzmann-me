f8f10350bcc33225b9869dfa7a511ad0f1b50b18
mkitzmann <mkitzmann@gmail.com>
2024-02-18 21:30:41 +0100

    add tailwind and nuxt pwa

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <a href="http://www.github.com/mkitzmann">
+      <img
+        src="/img/kitzmann-io-logo-web.jpeg"
+        alt="Moritz Kitzmann Logo"
+        class="max-h-64"
+      />
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+useHead({
+  link: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+      sizes: "48x48",
+    },
+  ],
+});
+</script>
